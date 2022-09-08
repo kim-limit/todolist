b0b9ed87dc94611f9186079b1bba5181df3fb6be
kim-limit <rlawogks2468@naver.com>
2022-09-08 11:18:06 +0900

    init git file

---
+++ src/components/TodoList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
+import TodoItem from "./TodoItem";
+
+const TodoListBlock = styled.div`
+  flex: 1; // 부모가 flex column일때 1하면 자신이 차지할수 있는 크기 꽉채움
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+  const todos = useTodoState();
+  return (
+    <TodoListBlock>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          done={todo.done}
+          text={todo.text}
+        />
+      ))}
+    </TodoListBlock>
+  );
+}
+
+export default TodoList;
